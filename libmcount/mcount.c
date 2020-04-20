a87193b3c85545f06dcf01156030d25ad1637eb5
Namhyung Kim <namhyung@gmail.com>
2020-04-20 15:17:44 +0900

    mcount: Suggest --flat option when segfault occurs
    Maybe it's because uftrace's fault.  As --flat is much safer option,
    it'd better for users to do it again with the option.
    
      $ uftrace ./thread-exit
      retval = 0x111
      handler() called!
      retval = (nil)
      WARN: Segmentation fault: address not mapped (addr: (nil))
      WARN:  if this happens only with uftrace, please consider --flat option.
    
      WARN: Backtrace from uftrace:
      WARN: =====================================
      WARN: [0] (main[55c1473f6912] <= __libc_start_main[7fb118bf7bbb])
      WARN: child terminated by signal: 11: Segmentation fault
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- libmcount/mcount.c
+++ libmcount/mcount.c
@@ -697,6 +697,11 @@ static void segv_handler(int sig, siginfo_t *si, void *ctx)
 		       sig, strsignal(sig), si->si_code);
 	}
 
+	if (!mcount_flat) {
+		pr_warn(" if this happens only with uftrace,"
+			" please consider --flat option.\n\n");
+	}
+
 	pr_warn("Backtrace from uftrace:\n");
 	pr_warn("=====================================\n");
 
