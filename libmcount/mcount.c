c47415a239e2cc40f684180ac853a0288ef0abf1
Namhyung Kim <namhyung@gmail.com>
2020-04-26 23:21:03 +0900

    mcount: Add an unit test case for estimated EXIT records
    When --estimate-return option is used, libmcount estimates function
    depth using frame pointer and inject EXIT records between each ENTRY.
    The mcount_estimate_return_depth test case is to verify the behavior
    using a local array as a frame pointer.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- libmcount/mcount.c
+++ libmcount/mcount.c
@@ -2047,4 +2047,46 @@ TEST_CASE(mcount_signal_setup)
 	return TEST_OK;
 }
 
+struct fake_rstack {
+	unsigned long *frame_pointer;
+	unsigned long func_addr;
+};
+
+TEST_CASE(mcount_estimate_return_depth)
+{
+	/* dummy frame pointer values - just to check relative values */
+	unsigned long frame_pointers[8];
+	/* increase idx/depth when frame pointer goes down */
+	struct fake_rstack test_scenario[] = {
+		{ &frame_pointers[7], 0x1234 },
+		{ &frame_pointers[4], 0x1234 },
+		{ &frame_pointers[0], 0x1234 },
+		{ &frame_pointers[4], 0x1234 },
+		{ &frame_pointers[5], 0x1234 },
+	};
+	/* mtdp->idx increased after mcount_entry() */
+	int depth_check[] = { 0, 1, 2, 1, 1 };
+	struct mcount_thread_data *mtdp;
+	unsigned i;
+
+	setup_mcount_test();
+	mtdp = mcount_prepare();
+	/* mcount_prepare calls mcount_guard_recursion() internally */
+	mcount_unguard_recursion(mtdp);
+
+	mcount_estimate_return = true;
+
+	for (i = 0; i < ARRAY_SIZE(test_scenario); i++) {
+		TEST_EQ(mcount_entry(test_scenario[i].frame_pointer,
+				     test_scenario[i].func_addr, NULL), 0);
+		TEST_EQ(mtdp->idx, depth_check[i] + 1);
+		TEST_EQ(mtdp->rstack[mtdp->idx - 1].depth, depth_check[i]);
+	}
+
+	cleanup_thread_data(mtdp);
+	mcount_cleanup();
+
+	return TEST_OK;
+}
+
 #endif /* UNIT_TEST */
